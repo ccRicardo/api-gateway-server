a133116ab9818d00d1e937fc4f70bef86a13977c
wyhGreat <471292562@qq.com>
2024-05-13 17:00:47 +0800

    对原代码进行了优化

---
+++ gateway-core/src/main/java/org/wyh/gateway/core/context/AttributeKey.java
@@ -0,0 +1,12 @@
+package org.wyh.gateway.core.context;
+
+/**
+ * @BelongsProject: api-gateway-server
+ * @BelongsPackage: org.wyh.gateway.core.context
+ * @Author: wyh
+ * @Date: 2024-05-13 16:59
+ * @Description:
+ */
+public class AttributeKey {
+
+}
