8d1d577ca3007a52909d50d2e3b9df798f531f7d
wyhGreat <471292562@qq.com>
2024-05-20 17:05:30 +0800

    对原代码进行了优化

--- gateway-core/src/main/java/org/wyh/gateway/core/Bootstrap.java
+++ gateway-core/src/main/java/org/wyh/gateway/core/Bootstrap.java
@@ -3,18 +3,21 @@ package org.wyh.gateway.core;
 import com.alibaba.fastjson.JSON;
 import lombok.extern.slf4j.Slf4j;
 import org.wyh.gateway.common.config.DynamicConfigManager;
+import org.wyh.gateway.common.config.Rule;
 import org.wyh.gateway.common.config.ServiceDefinition;
 import org.wyh.gateway.common.config.ServiceInstance;
 import org.wyh.gateway.common.constant.BasicConst;
 import org.wyh.gateway.common.utils.NetUtils;
 import org.wyh.gateway.common.utils.TimeUtil;
 import org.wyh.gateway.config.api.ConfigCenter;
+import org.wyh.gateway.config.api.RulesChangeListener;
 import org.wyh.gateway.core.config.Config;
 import org.wyh.gateway.core.config.ConfigLoader;
 import org.wyh.gateway.core.netty.Container;
 import org.wyh.gateway.register.api.RegisterCenter;
 import org.wyh.gateway.register.api.RegisterCenterListener;
 
+import java.util.List;
 import java.util.Map;
 import java.util.ServiceLoader;
 import java.util.Set;
@@ -28,9 +31,18 @@ import java.util.Set;
  */
 @Slf4j
 public class Bootstrap {
+    //配置中心，通过静态代码块初始化
+    private static final ConfigCenter configCenter;
+    //注册中心，通过静态代码块初始化
+    private static final RegisterCenter registerCenter;
+    //网关的规则变更监听器实例，通过静态代码块初始化
+    private static final RulesChangeListener gatewayRulesChangeListener;
+    //网关的注册中心监听器实例，用于监听服务的变更，通过静态代码块初始化
+    private static final RegisterCenterListener gatewayRegisterCenterListener;
+
     /**
      * @date: 2024-01-23 10:30
-     * @description: 网关系统启动方法
+     * @description: 核心方法，负责启动整个网关系统
      * @Param args:
      * @return: void
      */
@@ -39,31 +51,17 @@ public class Bootstrap {
         Config config = ConfigLoader.getInstance().load(args);
         log.info("api网关系统端口号：{}", config.getPort());
         //二、过滤插件初始化（插件初始化工作实际上是在过滤器链工厂类中完成的）
-        //三、初始化配置中心，然后监听规则配置的变更，并保存最新的规则配置
-        /*
-         * 以下这段代码的作用是通过java SPI机制来加载/构建配置中心实例
-         * SPI是JDK内置的一种服务提供发现机制，可以动态获取/发现接口的实现类
-         * 具体来说，服务提供者在提供了一种接口实现后，
-         * 需要在resources/META-INF/services目录下创建一个以接口（全类名）命名的文件
-         * 文件的内容就是该接口具体实现类的全类名
-         * 之后通过java.util.ServiceLoader，就可以根据文件中的实现类全类名，来构建/加载相应的实例
-         * ServiceLoader.findFirst方法返回的是第一个（种）实现类的实例。
-         */
-        ServiceLoader<ConfigCenter> serviceLoader = ServiceLoader.load(ConfigCenter.class);
-        final ConfigCenter configCenter = serviceLoader.findFirst().orElseThrow(() -> {
-            log.error("未找到配置中心实例");
-            return new RuntimeException("未找到配置中心实例");
-        });
+        //三、初始化配置中心，然后设置RulesChangeListener监听器实例，订阅规则的变更
         configCenter.init(config.getConfigAddress(), config.getEnv());
-        //RulesChangeListener是一个函数式接口，所以可以用lambda表达式来实现。其中rules代表新的规则配置列表。
-        configCenter.subscribeRulesChange(rules -> {
-            DynamicConfigManager.getInstance().putAllRule(rules);
-        });
+        configCenter.subscribeRulesChange(gatewayRulesChangeListener);
         //四、启动网关容器
         Container container = new Container(config);
         container.start();
-        //五、初始化注册中心，并监听服务的变更
-        final RegisterCenter registerCenter = registerAndSubscribe(config);
+        //五、初始化注册中心，然后注册网关服务，最后设置RegisterCenterListener监听器实例，订阅服务的变更
+        registerCenter.init(config.getRegistryAddress(), config.getEnv());
+        registerCenter.register(buildGatewayServiceDefinition(config),
+                buildGatewayServiceInstance(config));
+        registerCenter.subscribeAllServices(gatewayRegisterCenterListener);
         //六、服务优雅关机
         //addShutdownHook添加的线程会在JVM关闭之前被调用，因此通常用于在程序终止时执行一些清理工作。
         Runtime.getRuntime().addShutdownHook(new Thread(){
@@ -77,14 +75,23 @@ public class Bootstrap {
             }
         });
     }
-    /**
-     * @date: 2024-01-23 10:33
-     * @description: 初始化注册中心，并注册网关服务，然后监听服务的变更
-                     （网关服务也需要注册到注册中心）
-     * @Param config:
-     * @return: org.wyh.gateway.register.api.RegisterCenter
-     */
-    private static RegisterCenter registerAndSubscribe(Config config){
+    //静态代码块，负责初始化配置中心，注册中心，规则变更监听器和注册中心监听器属性
+    static{
+        /*
+         * 以下这段代码的作用是通过java SPI机制来加载/构建配置中心实例
+         * SPI是JDK内置的一种服务提供发现机制，可以动态获取/发现接口的实现类
+         * 具体来说，服务提供者在提供了一种接口实现后，
+         * 需要在resources/META-INF/services目录下创建一个以接口（全类名）命名的文件
+         * 文件的内容就是该接口具体实现类的全类名
+         * 之后通过java.util.ServiceLoader，就可以根据文件中的实现类全类名，来构建/加载相应的实例
+         * ServiceLoader.findFirst方法返回的是第一个（种）实现类的实例。
+         */
+        ServiceLoader<ConfigCenter> configCenters = ServiceLoader.load(ConfigCenter.class);
+        configCenter = configCenters.findFirst().orElseThrow(() -> {
+            //如果没找到实现类，则执行以下lambda表达式，抛出异常
+            log.error("未找到配置中心实例");
+            return new RuntimeException("未找到配置中心实例");
+        });
         /*
          * 以下这段代码的作用是通过java SPI机制来加载/构建注册中心实例
          * SPI是JDK内置的一种服务提供发现机制，可以动态获取/发现接口的实现类
@@ -94,20 +101,26 @@ public class Bootstrap {
          * 之后通过java.util.ServiceLoader，就可以根据文件中的实现类全类名，来构建/加载相应的实例
          * ServiceLoader.findFirst方法返回的是第一个（种）实现类的实例。
          */
-        ServiceLoader<RegisterCenter> serviceLoader = ServiceLoader.load(RegisterCenter.class);
-        final RegisterCenter registerCenter = serviceLoader.findFirst().orElseThrow(() -> {
+        ServiceLoader<RegisterCenter> registerCenters = ServiceLoader.load(RegisterCenter.class);
+        registerCenter = registerCenters.findFirst().orElseThrow(() -> {
+            //如果没找到实现类，则执行以下lambda表达式，抛出异常
             log.error("未找到注册中心实例");
             return new RuntimeException("未找到注册中心实例");
         });
-        //初始化注册中心
-        registerCenter.init(config.getRegistryAddress(), config.getEnv());
-        //构造网关服务的服务定义和服务实例
-        ServiceDefinition serviceDefinition = buildGatewayServiceDefinition(config);
-        ServiceInstance serviceInstance = buildGatewayServiceInstance(config);
-        //注册网关服务
-        registerCenter.register(serviceDefinition, serviceInstance);
-        //订阅所有服务。此处通过匿名内部类的方式实现了监听器接口中的方法。
-        registerCenter.subscribeAllServices(new RegisterCenterListener() {
+        /*
+         * 初始化规则变更监听器
+         */
+        gatewayRulesChangeListener = new RulesChangeListener() {
+            @Override
+            public void onRulesChange(List<Rule> rules) {
+                //rules代表新的规则配置列表。
+                DynamicConfigManager.getInstance().putAllRule(rules);
+            }
+        };
+        /*
+         * 初始化注册中心监听器
+         */
+        gatewayRegisterCenterListener = new RegisterCenterListener() {
             @Override
             public void onChange(ServiceDefinition serviceDefinition, Set<ServiceInstance> serviceInstanceSet) {
                 //日志输出
@@ -120,15 +133,16 @@ public class Bootstrap {
                 //将新的服务实例缓存到本地。
                 manager.addServiceInstance(serviceDefinition.getUniqueId(), serviceInstanceSet);
             }
-        });
-        return registerCenter;
+        };
     }
+    /*
+     * 以下是一些private修饰的辅助方法
+     */
     /**
-     * @date: 2024-01-23 10:41
-     * @description: 构造网关服务的服务定义
-    （网关服务也需要注册到注册中心）
+     * @date: 2024-05-20 17:01
+     * @description: 根据静态配置信息，构建网关的服务定义
      * @Param config:
-     * @return: org.wyh.common.config.ServiceDefinition
+     * @return: org.wyh.gateway.common.config.ServiceDefinition
      */
     private static ServiceDefinition buildGatewayServiceDefinition(Config config){
         ServiceDefinition serviceDefinition = new ServiceDefinition();
@@ -141,16 +155,15 @@ public class Bootstrap {
         return serviceDefinition;
     }
     /**
-     * @date: 2024-01-23 10:36
-     * @description: 构造网关服务的服务实例
-                     （网关服务也需要注册到注册中心）
-     * @Param config: 
-     * @return: org.wyh.common.config.ServiceInstance
+     * @date: 2024-05-20 17:02
+     * @description: 根据静态配置信息，构建网关的服务实例
+     * @Param config:
+     * @return: org.wyh.gateway.common.config.ServiceInstance
      */
     private static ServiceInstance buildGatewayServiceInstance(Config config){
+        ServiceInstance serviceInstance = new ServiceInstance();
         String localIp = NetUtils.getLocalIp();
         int port = config.getPort();
-        ServiceInstance serviceInstance = new ServiceInstance();
         //服务实例id的形式：ip:port
         serviceInstance.setServiceInstanceId(localIp + ":" + port);
         serviceInstance.setIp(localIp);
