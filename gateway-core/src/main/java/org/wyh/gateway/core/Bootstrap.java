907958aa1f042d43766c231751bdb0c8fdafab17
wyhGreat <471292562@qq.com>
2024-05-20 17:07:30 +0800

    对原代码进行了优化

--- gateway-core/src/main/java/org/wyh/gateway/core/Bootstrap.java
+++ gateway-core/src/main/java/org/wyh/gateway/core/Bootstrap.java
@@ -51,6 +51,7 @@ public class Bootstrap {
         Config config = ConfigLoader.getInstance().load(args);
         log.info("api网关系统端口号：{}", config.getPort());
         //二、过滤插件初始化（插件初始化工作实际上是在过滤器链工厂类中完成的）
+        // TODO: 2024-05-20 未完成过滤器插件的初始化 
         //三、初始化配置中心，然后设置RulesChangeListener监听器实例，订阅规则的变更
         configCenter.init(config.getConfigAddress(), config.getEnv());
         configCenter.subscribeRulesChange(gatewayRulesChangeListener);
